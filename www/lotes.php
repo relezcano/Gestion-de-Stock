7e55e09dc956b65523cfdc37652edfff906c6dc2
Ramiro Lezcano <relezcano369@gmail.com>
2018-09-27 19:29:17 -0300

    actualización

--- www/lotes.php
+++ www/lotes.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap-theme.css">
-    <link rel="stylesheet" href="css/clientes.css">
+    <link rel="stylesheet" href="css/lotes.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -17,26 +17,34 @@
   </head>
   <body>
 
-    <?php require 'include/menu_principal.php'; ?>
-
-    <div class="container" style="margin-top: 50px">
+    <div class="container" style="margin-top: 20px">
       <div class="row">
         <div class="col-sm-2">
-          <button type="button" name="agregar" class="btn btn-primary" onclick="window.open('nuevo_cliente.php');" target="_blank"><strong>Nuevo Cliente</strong></button>
+          <button type="button" name="agregar" class="btn btn-primary" onclick="location.href = 'nuevo_lote.php';"><strong><span class="glyphicon glyphicon-plus"></span> Nuevo Lote</strong></button>
         </div>
+
+        <form class="btnmodificar" action="lotes.php" method="post">
         <div class="col-sm-2">
-          <button type="button" name="modificar" class="btn btn-warning"><strong>Modificar Cliente</strong></button>
+          <button type="submit" name="modificar" class="btn btn-warning"><strong><span class="glyphicon glyphicon-pencil"></span> Modificar Lote</strong></button>
         </div>
-        <div class="col-sm-2">
-          <button type="button" name="eliminar" class="btn btn-danger"><strong>Eliminar Cliente</strong></button>
-        </div>
-        <div class="col-sm-3"></div>
-        <div class="col-sm-3">
+          <div class="col-sm-1">
+            <input style="margin-left: 12px" class="form-control" type="text" name="select" placeholder="ID">
+          </div>
+        </form>
+
+        <form class="eliminar" action="productos.php" method="post">
+          <div class="col-sm-2">
+            <button type="submit" name="eliminar" class="btn btn-danger"><strong><span class="glyphicon glyphicon-trash"></span> Eliminar Lote</strong></button>
+          </div>
+          <div class="col-sm-1">
+            <input style="margin-left: 5px" class="form-control" type="text" name="select2" placeholder="ID">
+          </div>
+        </form>
+
+        <div class="col-sm-4">
           <input class="form-control" id="myInput" type="text" name="filtro" placeholder="Filtrar por...">
-          <!-- <button type="submit" name="buscar" class="btn btn-info"><span class="glyphicon glyphicon-search"></span></button> -->
         </div>
       </div>
-
 
     <div class="row">
       <div class="col-lg-12">
@@ -44,9 +52,7 @@
 
           require 'include/conexion.php';
 
-        
-
-          $sql = ("SELECT * FROM clients ORDER BY id DESC");
+          $sql = ("SELECT * FROM Lote ORDER BY id_L ASC");
           $ret = $db->query($sql);
 
           ?>
@@ -56,28 +62,28 @@
               <thead class="thead-dark">
                 <tr>
                   <th scope="col">ID</th>
-                  <th scope="col">Producto</th>
                   <th scope="col">Cantidad</th>
-                  <th scope="col">N° Comprobante</th>
-                  <th scope="col">$ Compra</th>
-                  <th scope="col">Proveedor</th>
-                  <th scope="col">Fecha Compra</th>
+                  <th scope="col">Precio Compra</th>
+                  <th scope="col">Fecha Alta</th>
                   <th scope="col">Fecha Vencimiento</th>
+                  <th scope="col">N° Comprobante</th>
+                  <th scope="col">ID Producto</th>
+                  <th scope="col">ID Proveedor</th>
                   <th scope="col">Observaciones</th>
                 </tr>
               </thead>
               <tbody id="myTable">
                 <tr>
-                  <? while($row = $ret->fetchArray(SQLITE3_ASSOC)){?>
-                    <td><?//echo $row['id'];?></td>
-                    <td><?//echo $row['name'];?></td>
-                    <td><?//echo $row['phone'];?></td>
-                    <td><?//echo $row['dateIn'];?></td>
-                    <td><?//echo $row['dateIn'];?></td>
-                    <td><?//echo $row['dateIn'];?></td>
-                    <td><?//echo $row['dateIn'];?></td>
-                    <td><?//echo $row['dateIn'];?></td>
-                    <td><?//echo $row['dateIn'];?></td>
+                  <? while($row = $ret->fetchArray(SQLITE3_ASSOC)){ ?>
+                    <td><?echo $row['id_L'];?></td>
+                    <td><?echo $row['cant'];?></td>
+                    <td><?echo $row['price_C'];?></td>
+                    <td><?echo $row['date_Alt'];?></td>
+                    <td><?echo $row['date_Ven'];?></td>
+                    <td><?echo $row['n_Comp'];?></td>
+                    <td><?echo $row['id_Prod1'];?></td>
+                    <td><?echo $row['id_Prov1'];?></td>
+                    <td><?echo $row['obs_L'];?></td>
                 </tr>
                 <?}?>
               </tbody>
@@ -86,10 +92,182 @@
         </div>
         </div>
       </div>
-    <?php require 'include/footer.php'; ?>
     </div>
 
+<!-- Modificar Cliente -->
+<?
+if (isset($_POST['modificar'])) {
+  if (isset($_POST['select'])) {
+
+
+    $idSel = $_POST['select'];
+
+    $sql = ("SELECT * FROM Lote WHERE id_L = '$idSel'");
+    $ret = $db->query($sql);
+
+    while($row = $ret->fetchArray(SQLITE3_ASSOC)){
+      $id = $row['id_L'];
+      $cant = $row['cant'];
+      $price = $row['price_C'];
+      $dateA = $row['date_Alt'];
+      $dateV = $row['date_Ven'];
+      $comp = $row['n_Comp'];
+      $obs = $row['obs_L'];
+      $idProd = $row['id_Prod1'];
+      $idProv = $row['id_Prov1'];
+
+
+?>
+
+<div class="container-fluid" style="margin-top: 50px">
+  <div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-6" style="margin-left: 7px">
+      <h1 style="font-size: 35px; color: #ffffff; padding-left: 35px"><strong>Modificar Lote</strong></h1>
+    </div>
+    <div class="col-sm-5"></div>
+  </div>
+
+  <div class="formulario">
+
+  <form class="modificar" action="modificar_lote.php" method="post" style="margin-left: 50px">
+
+    <input type="hidden" name="id_Lote" value="<?echo $id;?>">
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-3">
+        <label style="margin-top: 5px">Cantidad</label>
+      </div>
+      <div class="col-sm-3">
+        <input class="form-control" style="width: 136px; margin-bottom: 5px" type="text" name="cant" value="<?echo $cant;?>">
+      </div>
+      <div class="col-sm-5"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-3">
+        <label style="margin-top: 5px">Precio Compra</label>
+      </div>
+      <div class="col-sm-3">
+        <input class="form-control" style="width: 136px; margin-bottom: 5px" type="number" name="price_C" value="<?echo $price;?>">
+      </div>
+      <div class="col-sm-5"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-3">
+        <label style="margin-top: 5px">Fecha Alta</label>
+      </div>
+      <div class="col-sm-3">
+        <input class="form-control" style="width: 160px; margin-bottom: 5px" type="text" name="date_Alt" value="<?echo $dateA;?>">
+      </div>
+      <div class="col-sm-5"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-3">
+        <label style="margin-top: 5px">Vencimiento</label>
+      </div>
+      <div class="col-sm-3">
+        <input class="form-control" style="width: 160px; margin-bottom: 5px" type="text" name="date_Ven" value="<?echo $dateV;?>">
+      </div>
+      <div class="col-sm-5"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-3">
+        <label style="margin-top: 5px">N° Comprobante</label>
+      </div>
+      <div class="col-sm-3">
+        <input class="form-control" style="width: 210px; margin-bottom: 5px" type="text" name="n_Comp" value="<?echo $comp;?>">
+      </div>
+      <div class="col-sm-5"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-3">
+        <label style="margin-top: 5px">ID Producto</label>
+      </div>
+      <div class="col-sm-3">
+        <input class="form-control" style="width: 100px; margin-bottom: 5px" type="number" name="id_Prod1" value="<?echo $idProd;?>">
+      </div>
+      <div class="col-sm-5"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-3">
+        <label style="margin-top: 5px">ID Proveedor</label>
+      </div>
+      <div class="col-sm-3">
+        <input class="form-control" style="width: 100px; margin-bottom: 5px" type="number" name="id_Prov1" value="<?echo $idProv;?>">
+      </div>
+      <div class="col-sm-5"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-3">
+        <label style="margin-top: 5px">Observaciones</label>
+      </div>
+      <div class="col-sm-3">
+        <textarea class="form-control" name="obs_Prod" rows="8" cols="60" style="width: 380px"><?echo $obs;?></textarea>
+      </div>
+      <div class="col-sm-5"></div>
+    </div>
+
+    <br>
+
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-3">
+        <button style="font-family: verdana; font-weight: bolder" type="submit" name="guardar" class="btn btn-success btn-md"><span class="glyphicon glyphicon-floppy-disk"></span><strong> GUARDAR</strong></button>
+      </div>
+      <div class="col-md-3">
+        <button style="margin-left: 30px; font-family: verdana; font-weight: bolder" type="button" name="cancelar" onclick="location.href = 'lotes.php';" class="btn btn-danger btn-md"><span class="glyphicon glyphicon-remove"></span><strong> CANCELAR</strong></button>
+      </div>
+        <div class="col-md-3"></div>
+      </div>
+    </form>
+    </div>
+<br><br>
+<?
+
+  }
+}
+}
+?>
+
+<!-- Eliminar Cliente -->
+
+<?
+  if(isset($_POST['eliminar'])){
+    if (isset($_POST['select2'])) {
+
+      $idSel2 = $_POST['select2'];
+      $sql = ("DELETE FROM Lote WHERE id_L = $idSel2");
+      $ret = $db->query($sql);
+      echo "Lote eliminado exitosamente!!!";
+
+      ?><script>
+        window.location.replace('lotes.php');  // redireccionar a otra pagina.
+      </script><?
+
+    } else {
+        echo "Seleccione una ID para poder eliminar";
+    }
+  }
+
+?>
+
     <script>
+
       $(document).ready(function(){
         $("#myInput").on("keyup", function() {
           var value = $(this).val().toLowerCase();
@@ -98,6 +276,7 @@
           });
         });
       });
+
     </script>
 
   </body>
