e7b4de5e5e498fa0a948ceedf980393769d8f1be
Ramiro Lezcano <relezcano369@gmail.com>
2018-10-17 17:23:15 -0300

    Add files via upload

--- www/include/menu_principal.php
+++ www/include/menu_principal.php
@@ -1,19 +1,19 @@
 <div class="container-fluid" style="background-color: #4d4d4d; padding: 5px">
-  <button class="btn btn-warning" type="button" onclick="window.open('ventas.php');" target="_blank"><strong>Ventas</strong></button>
-  <button class="btn btn-warning" type="button" onclick="window.open('stock.php');" target="_blank"><strong>Stock</strong></button>
-  <button class="btn btn-warning" type="button" onclick="window.open('ant_prov.php');" target="_blank"><strong>Anticipo de Proveedores</strong></button>
-  <button class="btn btn-warning" type="button" onclick="window.open('ing_mer.php');" target="_blank"><strong>Ingreso de Mercaderia</strong></button>
-  <button class="btn btn-warning" type="button" onclick="window.open('deudores.php');" target="_blank"><strong>Deudores</strong></button>
+  <button class="btn btn-warning" type="button" onclick="window.open('../ventas.php');" target="_blank"><strong>Ventas</strong></button>
+  <button class="btn btn-warning" type="button" onclick="window.open('../stock.php');" target="_blank"><strong>Stock</strong></button>
+  <button class="btn btn-warning" type="button" onclick="window.open('../ant_prov.php');" target="_blank"><strong>Anticipo de Proveedores</strong></button>
+  <button class="btn btn-warning" type="button" onclick="window.open('../ing_mer.php');" target="_blank"><strong>Ingreso de Mercaderia</strong></button>
+  <button class="btn btn-warning" type="button" onclick="window.open('../deudores.php');" target="_blank"><strong>Deudores</strong></button>
   <div class="dropdown">
     <button class="btn btn-warning"><strong>Gestión de Stock</strong></button>
     <div class="dropdown-content" style="background-color: #fff7e6; border-top-right-radius: 45px; border-bottom-right-radius: 30px; border-bottom-left-radius: 30px; width: 175px">
-      <a style="border-top-right-radius: 100px" href="productos.php" target="_blank"><strong>Productos</strong></a>
-      <a href="#" target="_blank"><strong>Proveedores</strong></a>
-      <a href="lotes.php" target="_blank"><strong>Lotes</strong></a>
-      <a href="#" target="_blank"><strong>Pedidos</strong></a>
-      <a href="#" target="_blank"><strong>Ventas</strong></a>
-      <a href="clientes.php" target="_blank"><strong>Clientes</strong></a>
-      <a style="border-bottom-left-radius: 30px; border-bottom-right-radius: 30px" href="deudas.php" target="_blank"><strong>Deudas</strong></a>
+      <a style="border-top-right-radius: 100px" href="../productos.php" target="_blank"><strong>Productos</strong></a>
+      <a href="../proveedores.php" target="_blank"><strong>Proveedores</strong></a>
+      <a href="../lotes.php" target="_blank"><strong>Lotes</strong></a>
+      <a href="../pedidos.php" target="_blank"><strong>Pedidos</strong></a>
+      <a href="../ventas.php" target="_blank"><strong>Ventas</strong></a>
+      <a href="../clientes.php" target="_blank"><strong>Clientes</strong></a>
+      <a style="border-bottom-left-radius: 30px; border-bottom-right-radius: 30px" href="../deudas.php" target="_blank"><strong>Deudas</strong></a>
     </div>
   </div>
 </div>
